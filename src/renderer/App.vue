b44e7695e31c059ec4adeae665544d218e808d29
ovack <youfengtec@gmail.com>
2021-10-24 10:25:02 +0800

    init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'subtitles-knife'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
